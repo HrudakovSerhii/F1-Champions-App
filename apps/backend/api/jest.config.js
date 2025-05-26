6b98b0b1c49784f700655b52823dec08fa8652bb
Serhii <avatarcapa@gmail.com>
2025-05-26 09:17:47 +0200

    TS config for BE app

---
+++ apps/backend/api/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'backend-api',
+  preset: '../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/apps/backend/api',
+};
