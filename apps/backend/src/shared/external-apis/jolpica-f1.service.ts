39405cbeb21f948cd6b9582e972814c135563027
Serhii <avatarcapa@gmail.com>
2025-06-04 17:33:05 +0200

    Renamed External API url to align with documentation Fixed return data types for getSeasonRaceWinners and getSeasonsWinners functions

--- apps/backend/src/shared/external-apis/jolpica-f1.service.ts
+++ apps/backend/src/shared/external-apis/jolpica-f1.service.ts
@@ -3,9 +3,12 @@ import { HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
 import { firstValueFrom } from 'rxjs';
 
-import type { RaceWinnersResponse, SeasonChampionsResponse } from '../../types';
+import type {
+  JolpiRaceTableMRData,
+  JolpiDriverStandingMRData,
+} from '../../types';
 
-import { ERGAST_API } from '../../constants/constants';
+import { JOLPI_API_BASE_URL } from '../../constants/constants';
 
 @Injectable()
 export class JolpicaF1Service {
@@ -17,92 +20,65 @@ export class JolpicaF1Service {
     private readonly configService: ConfigService
   ) {
     this.baseUrl = this.configService.get<string>(
-      'JOLPICA_F1_API_URL',
-      ERGAST_API
+      'JOLPI_API_BASE_URL',
+      JOLPI_API_BASE_URL
     );
   }
 
-  async getSeasonChampions(
+  async getSeasonsWinners(
     limit?: number,
-    offset?: number,
-    season?: string
-  ): Promise<SeasonChampionsResponse | null> {
+    offset?: number
+  ): Promise<JolpiRaceTableMRData | null> {
     try {
       const params = new URLSearchParams();
+
       if (limit) params.append('limit', limit.toString());
       if (offset) params.append('offset', offset.toString());
-      if (season) params.append('season', season);
 
-      const url = `${this.baseUrl}/driverStandings/1.json?${params.toString()}`;
-      this.logger.debug(`Fetching season champions from: ${url}`);
+      // https://api.jolpi.ca/ergast/f1/results/
+      const url = `${this.baseUrl}/results`;
 
       const response = await firstValueFrom(
-        this.httpService.get<SeasonChampionsResponse>(url)
+        this.httpService.get<JolpiRaceTableMRData>(url)
       );
 
-      this.logger.log(`Successfully fetched season champions data`);
       return response.data;
     } catch (error) {
       this.logger.error(
         'Failed to fetch season champions from Jolpica F1 API:',
         error
       );
+
       return null;
     }
   }
 
-  async getRaceWinners(
+  async getSeasonRaceWinners(
     season: string,
     limit?: number,
     offset?: number
-  ): Promise<RaceWinnersResponse | null> {
+  ): Promise<JolpiDriverStandingMRData | null> {
     try {
       const params = new URLSearchParams();
+
       if (limit) params.append('limit', limit.toString());
       if (offset) params.append('offset', offset.toString());
 
-      const url = `${
-        this.baseUrl
-      }/${season}/results/1.json?${params.toString()}`;
-      this.logger.debug(
-        `Fetching race winners for season ${season} from: ${url}`
-      );
+      // https://api.jolpi.ca/ergast/f1/2024/driverstandings/
+      const url = `${this.baseUrl}${season}/driverstandings`;
 
       const response = await firstValueFrom(
-        this.httpService.get<RaceWinnersResponse>(url)
+        this.httpService.get<JolpiDriverStandingMRData>(url)
       );
 
-      this.logger.log(
-        `Successfully fetched race winners data for season ${season}`
-      );
       return response.data;
     } catch (error) {
       this.logger.error(
         `Failed to fetch race winners for season ${season} from Jolpica F1 API:`,
         error
       );
+
       return null;
     }
   }
-
-  // async getSeasons(): Promise<string[]> {
-  //   try {
-  //     const url = `${this.baseUrl}/seasons.json?limit=100`;
-  //     this.logger.debug(`Fetching seasons from: ${url}`);
-  //
-  //     const response = await firstValueFrom(
-  //       this.httpService.get<SeasonsResponse>(url)
-  //     );
-  //
-  //     const seasons =
-  //       response.data?.MRData?.SeasonTable?.Seasons?.map(
-  //         (season) => season.season
-  //       ) || [];
-  //     this.logger.log(`Successfully fetched ${seasons.length} seasons`);
-  //     return seasons;
-  //   } catch (error) {
-  //     this.logger.error('Failed to fetch seasons from Jolpica F1 API:', error);
-  //     return [];
-  //   }
-  // }
 }
